2a42bc1e2b4419d63b797fd75a739a127cd9eb14
vzmvc <103417507+vzmvc@users.noreply.github.com>
2025-04-20 22:04:51 +0200

    finished exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    var concat = "";
+    for (i=0 ; i < num; i++){
+        concat = concat+str;
+    }
+    return concat;    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    var reverse = "";
+    for (var i = input.length-1; i >= 0 ; i--){
+        reverse = reverse+input[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
